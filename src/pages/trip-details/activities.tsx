532d3bc5f0a7795d05ca0d83597bf53829c700cc
Renan <renan.fachin@hotmail.com>
2024-07-14 19:26:25 -0300

    feat: Get travel details data - activities and guests

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,38 +1,74 @@
 import { CircleCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+import { ptBR } from 'date-fns/locale'
+
+interface Activity {
+  date: string
+  activities: {
+    id: string
+    title: string
+    occurs_at: string
+  }[]
+}
 
 export function Acitivities(){
+  const params = useParams() // tripID
+  const [activities, setActivities] = useState<Activity[]>([])
+  
+  useEffect(() => {
+    api.get(`/trips/${params.tripId}/activities`)
+    .then(response => setActivities(response.data.activities))
+  },[params.tripId])
+  
+
+  
   return(
     <div className="space-y-8">
-            <div className="space-y-2.5">
-              <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-                <span className="text-xs text-zinc-500">Sábado</span>
-              </div>
-
-              <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nesta data.</p>
-            </div>
-
-            <div className="space-y-2.5">
-              <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-                <span className="text-xs text-zinc-500">Domingo</span>
-              </div>
-
-              <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                  <CircleCheck className="size-5 text-lime-300"/>
-                  <span className="text-zinc-100">Academia em grupo</span>
-                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+          {
+            activities.map(activity => {
+            return(
+              <div key={activity.date} className="space-y-2.5">
+                <div className="flex gap-2 items-baseline">
+                  <span className="text-xl text-zinc-300 font-semibold">
+                    Dia {format(activity.date, 'd')}
+                  </span>
+                  <span className="text-xs text-zinc-500">
+                    {format(activity.date, 'EEEE', {
+                      locale: ptBR
+                    })}
+                  </span>
                 </div>
+
+               {
+                activity.activities.length > 0 ? (
+                  <div  className="space-y-2.5">
+                    {
+                      activity.activities.map(event => {
+                        return(
+                          <div key={event.id} className="space-y-2.5">
+                            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3" >
+                              <CircleCheck className="size-5 text-lime-300"/>
+                              <span className="text-zinc-100">{event.title}</span>
+                              <span className="text-zinc-400 text-sm ml-auto">
+                                {format(event.occurs_at, 'HH:mm')}h
+                              </span>
+                            </div>
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                ) : (
+                  <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nesta data.</p>
+                )
+               }
               </div>
-              <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                  <CircleCheck className="size-5 text-lime-300"/>
-                  <span className="text-zinc-100">Almoço</span>
-                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
-                </div>
-              </div>
-            </div>
-          </div>
+            )
+            })
+          }
+    </div>
   )
 }
--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,10 +1,31 @@
 import { Calendar, Tag, X } from "lucide-react";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import {  useParams } from "react-router-dom";
 
 interface CreateActivityModalProps{
   closeCreateActivityModal: () => void
 }
 
 export function CreateActivityModal({closeCreateActivityModal}: CreateActivityModalProps){
+  const params = useParams() // tripID
+
+
+  async function createActivity(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    const title = data.get('title')?.toString()
+    const occurs_at = data.get('occurs_at')?.toString()
+
+    await api.post(`/trips/${params.tripId}/activities`, {
+      title,
+      occurs_at
+    })
+
+    window.document.location.reload()
+  }
+
   return(
 <div className='fixed inset-0 bg-black/65 flex items-center justify-center'>
             <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
@@ -21,7 +42,7 @@ export function CreateActivityModal({closeCreateActivityModal}: CreateActivityMo
                 </p>
               </div>
 
-              <form className='space-y-3'>
+              <form onSubmit={createActivity} className='space-y-3'>
                 <div className='h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 px-6 flex-1'>
                   <Tag className='size-5 text-zinc-400'/>
                   <input 
